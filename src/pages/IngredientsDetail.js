51031d81cffb7a42a4092fb36cd9a776e70614a6
zzgh06 <zzgh06@naver.com>
2024-06-24 19:51:06 +0900

    ingredient-detail-page react-daum-postcode add

--- src/pages/IngredientsDetail.js
+++ src/pages/IngredientsDetail.js
@@ -1,72 +1,74 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchIngredients } from "../redux/ingredientSlice";
-import {
-  Button,
-  Carousel,
-  Col,
-  Container,
-  Dropdown,
-  Row,
-} from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getIngredient } from "../redux/ingredientSlice";
+import { Col, Container, Row } from "react-bootstrap";
 import { currencyFormat } from "../utils/number";
 import "../style/IngredientsDetail.style.css";
 import AddressInput from "../component/AddressInput/AddressInput";
+import DeliveryEstimate from "../component/DeliveryEstimate/DeliveryEstimate";
 
 const IngredientsDetail = () => {
   const { id } = useParams();
-  const ingredientId = id;
   const dispatch = useDispatch();
   const [address, setAddress] = useState("지역을 선택해주세요");
-  const ingredientList = useSelector(
-    (state) => state.ingredients.ingredients || []
+  const selectedIngredient = useSelector(
+    (state) => state.ingredients.selectedIngredient || []
   );
-  const ingredient = ingredientList.filter((item) => item?._id === id);
 
   useEffect(() => {
-    dispatch(fetchIngredients());
-  }, []);
-
-  const recentlyIngredient = {
-    id: ingredient[0]._id,
-    name: ingredient[0].name,
-    image: ingredient[0].image,
-  };
+    dispatch(getIngredient(id));
+  }, [dispatch, id]);
 
   useEffect(() => {
+    if (!selectedIngredient._id) return;
+
+    const recentlyIngredient = {
+      id: selectedIngredient._id,
+      name: selectedIngredient.name,
+      image: selectedIngredient.image,
+    };
+
     const viewedIngredients =
       JSON.parse(localStorage.getItem("viewedIngredients")) || [];
 
     const updatedViewedIngredients = viewedIngredients.filter(
-      (item) => item.id !== id
+      (item) => item.id !== selectedIngredient._id
     );
+
     updatedViewedIngredients.unshift(recentlyIngredient);
 
     localStorage.setItem(
       "viewedIngredients",
       JSON.stringify(updatedViewedIngredients.slice(0, 2))
     );
-  }, [ingredientId, recentlyIngredient]);
+  }, [selectedIngredient]);
 
-  console.log(ingredient[0]);
   return (
     <div>
       <Container fluid className="ingredient-container">
         <Row>
           <Col lg={6} className="image-container">
-            <img src={ingredient[0].image} alt={ingredient[0].name} className="img-fluid" />
+            <img
+              src={selectedIngredient.image}
+              alt={selectedIngredient.name}
+              className="img-fluid"
+            />
           </Col>
           <Col lg={6} className="ingredient-info-area">
-            <div className="ingredient-name">{ingredient[0].name}</div>
-            <div className="ingredient-price"><span className="discount-rate">30% </span>{currencyFormat(ingredient[0].price)}원</div>
-            <div className="ingredient-info">{ingredient[0].description}</div>
+            <div className="ingredient-name">{selectedIngredient.name}</div>
+            <div className="ingredient-price">
+              <span className="discount-rate">30% </span>
+              {currencyFormat(selectedIngredient.price)}원
+            </div>
+            <div className="ingredient-info">
+              {selectedIngredient.description}
+            </div>
             <div className="delivery-info">
               <div>배송안내</div>
               <h6>{address}</h6>
               <AddressInput setAddress={setAddress} />
-              {/* <DeliveryEstimate address={address} /> */}
+              <DeliveryEstimate address={address} />
             </div>
           </Col>
         </Row>
--- src/redux/ingredientSlice.js
+++ src/redux/ingredientSlice.js
@@ -1,60 +1,98 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../utils/api";
 
-export const fetchIngredients = createAsyncThunk('ingredients/fetchIngredients', async (searchQuery) => {  
-  const response = await api.get(`/ingredient?name=${searchQuery.name}&page=${searchQuery.page}`);
-  return response.data;
-});
+export const fetchIngredients = createAsyncThunk(
+  "ingredients/fetchIngredients",
+  async (searchQuery) => {
+    const response = await api.get(
+      `/ingredient?name=${searchQuery.name}&page=${searchQuery.page}`
+    );
+    return response.data;
+  }
+);
 
-export const createIngredient = createAsyncThunk('ingredients/createIngredient', async (ingredient) => {
-  const response = await api.post('/ingredient', ingredient);
-  return response.data;
-});
+export const getIngredient = createAsyncThunk(
+  "ingredients/getIngredient",
+  async (id) => {
+    const response = await api.get(`/ingredient/${id}`);
+    return response.data;
+  }
+);
 
-export const editIngredient = createAsyncThunk('ingredients/editIngredient', async ({ id, ingredient }) => {
-  const response = await api.put(`/ingredient/${id}`, ingredient);
-  return response.data;
-});
+export const createIngredient = createAsyncThunk(
+  "ingredients/createIngredient",
+  async (ingredient) => {
+    const response = await api.post("/ingredient", ingredient);
+    return response.data;
+  }
+);
 
-export const deleteIngredient = createAsyncThunk('ingredients/deleteIngredient', async (id) => {
-  const response = await api.delete(`/ingredient/${id}`);
-  return response.data;
-});
+export const editIngredient = createAsyncThunk(
+  "ingredients/editIngredient",
+  async ({ id, ingredient }) => {
+    const response = await api.put(`/ingredient/${id}`, ingredient);
+    return response.data;
+  }
+);
+
+export const deleteIngredient = createAsyncThunk(
+  "ingredients/deleteIngredient",
+  async (id) => {
+    const response = await api.delete(`/ingredient/${id}`);
+    return response.data;
+  }
+);
 
 const ingredientSlice = createSlice({
-  name: 'ingredients',
+  name: "ingredients",
   initialState: {
     ingredients: [],
+    selectedIngredient: null,
     totalPages: 0,
-    status: 'idle',
+    status: "idle",
     error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchIngredients.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchIngredients.fulfilled, (state, action) => {
-        state.status = 'succeeded';
+        state.status = "succeeded";
         state.ingredients = action.payload.data.ingredients;
         state.totalPages = action.payload.data.totalPageNum;
       })
       .addCase(fetchIngredients.rejected, (state, action) => {
-        state.status = 'failed';
+        state.status = "failed";
         state.error = action.error.message;
       })
       .addCase(createIngredient.fulfilled, (state, action) => {
         state.ingredients.push(action.payload.data);
       })
       .addCase(editIngredient.fulfilled, (state, action) => {
-        const index = state.ingredients.findIndex(ingredient => ingredient._id === action.payload.ingredient._id);
+        const index = state.ingredients.findIndex(
+          (ingredient) => ingredient._id === action.payload.ingredient._id
+        );
         if (index !== -1) {
           state.ingredients[index] = action.payload.ingredient;
         }
-      })      
+      })
       .addCase(deleteIngredient.fulfilled, (state, action) => {
-        state.ingredients = state.ingredients.filter(ingredient => ingredient._id !== action.payload.ingredient._id);
+        state.ingredients = state.ingredients.filter(
+          (ingredient) => ingredient._id !== action.payload.ingredient._id
+        );
+      })
+      .addCase(getIngredient.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getIngredient.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.selectedIngredient = action.payload.ingredient;
+      })
+      .addCase(getIngredient.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
       });
   },
 });
