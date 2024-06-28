8eaffd5e16a7288f0a2d021024895a93acdcdd9c
zzgh06 <zzgh06@naver.com>
2024-06-28 19:07:01 +0900

    edit route

--- src/component/IngredientCard/IngredientCard.js
+++ src/component/IngredientCard/IngredientCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../style/IngredientCard.style.css";
+import "../../style/ingredientCard.style.css";
 import { useNavigate } from "react-router-dom";
 
 const IngredientCard = ({item}) => {
