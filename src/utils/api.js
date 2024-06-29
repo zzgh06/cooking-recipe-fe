26d31ca150d5164cdcc26b5b13c6a2fd94e95e74
Leejeongwoo <juny1alex@gmail.com>
2024-06-30 00:22:14 +0900

    fix: query error

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 // 상황따라 주소 다름
-// const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
+const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 // const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+//const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-  baseURL: `${BACKEND_PROXY}/api`,
+  baseURL: `${LOCAL_BACKEND}/api`,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
