52d0a302d2b16ecd42da6633725f72b2f3d2101c
zzgh06 <zzgh06@naver.com>
2024-07-12 03:36:06 +0900

    update-recipe-feature

--- src/component/SearchBox/SerachBox.js
+++ src/component/SearchBox/SerachBox.js
@@ -1,31 +1,60 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { fetchRecommendedRecipes } from '../../redux/fridgeSlice';
 
-const SearchBox = ({searchQuery, setSearchQuery, placeholder, field, page='store'}) => {
+const SearchBox = ({ searchQuery, setSearchQuery, placeholder, field, page = 'store', selectedIngredients }) => {
+  const dispatch = useDispatch();
   const [query] = useSearchParams();
-  const [keyword, setKeyword] = useState(query.get(field) || "");
-  let navigate = useNavigate()
+  const [keyword, setKeyword] = useState(query.get(field) || '');
 
+  // 선택된 재료들이 변경될 때마다 검색 상자에 표시할 키워드 업데이트
+  useEffect(() => {
+    if (selectedIngredients?.length > 0) {
+      setKeyword(selectedIngredients.join(', '));
+    } else {
+      setKeyword('');
+    }
+  }, [selectedIngredients]);
+
+  // Enter 키 입력 시 검색 실행
   const onCheckEnter = (event) => {
-    if (event.key === "Enter") {
-      setSearchQuery({ ...searchQuery, page: 1, [field]: event.target.value });
+    if (event.key === 'Enter') {
+      handleSearch(event.target.value);
     }
   };
-  
+
+  // 검색 버튼 클릭 시 검색 실행
+  const handleSearch = (keyword) => {
+    if (keyword.trim() === '') {
+      // 검색어가 비어 있으면 상태 초기화
+      setKeyword('');
+      setSearchQuery({ ...searchQuery, page: 1, [field]: '' });
+    } else {
+      setSearchQuery({ ...searchQuery, page: 1, [field]: keyword });
+      dispatch(fetchRecommendedRecipes(selectedIngredients));
+    }
+  };
+
+  // 검색어 입력 시 키워드 상태 업데이트
+  const handleInputChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
   return (
     <div className="search-box">
-      <FontAwesomeIcon icon={faSearch} />
+      <FontAwesomeIcon icon={faSearch} onClick={() => handleSearch(keyword)}/>
       <input
         type="text"
         placeholder={placeholder}
         onKeyPress={onCheckEnter}
-        onChange={(event) => setKeyword(event.target.value)}
+        onChange={handleInputChange}
         value={keyword}
       />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
--- src/pages/IngredientsDetail.js
+++ src/pages/IngredientsDetail.js
@@ -1,31 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams, useNavigate } from "react-router-dom";
-import { getIngredient } from "../redux/ingredientSlice";
-import { Col, Container, Row, Button } from "react-bootstrap";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { getIngredient, getIngredientByName } from "../redux/ingredientSlice";
+import { Container, Grid, Typography, Button, Box } from "@mui/material";
 import { currencyFormat } from "../utils/number";
-import "../style/IngredientsDetail.style.css";
 import AddressInput from "../component/AddressInput/AddressInput";
 import DeliveryEstimate from "../component/DeliveryEstimate/DeliveryEstimate";
 import Review from "../component/Review/Review";
-//import { addItemToCart } from "../redux/cartSlice";
 import { addItemToCart } from "../redux/cartSlice";
+
 const IngredientsDetail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => {
-    return state.auth;
-  });
+  const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const [address, setAddress] = useState("지역을 선택해주세요");
   const selectedIngredient = useSelector(
-    (state) => state.ingredients.selectedIngredient || []
+    (state) => state.ingredients.selectedIngredient || {}
   );
 
   useEffect(() => {
-    dispatch(getIngredient(id));
-  }, [dispatch, id]);
+    const queryParams = new URLSearchParams(location.search);
+    const ingredientName = queryParams.get("name");
+
+    if (ingredientName) {
+      dispatch(getIngredientByName(ingredientName));
+    } else {
+      dispatch(getIngredient(id));
+    }
+  }, [dispatch, id, location.search]);
 
   useEffect(() => {
     if (!selectedIngredient._id) return;
@@ -51,49 +56,67 @@ const IngredientsDetail = () => {
     );
   }, [selectedIngredient]);
 
-  //console.log("selectedIngredient id", selectedIngredient._id);
   const addCart = () => {
     if (!user) navigate("/login");
     dispatch(addItemToCart({ ingredientId: id }));
   };
+
+  const goHome = () => {
+    navigate('/');
+  };
+
   return (
-    <div>
-      <Container fluid className="ingredient-container">
-        <Row>
-          <Col lg={6} className="image-container">
-            <img
-              src={selectedIngredient.image}
-              alt={selectedIngredient.name}
-              className="img-fluid"
-            />
-          </Col>
-          <Col lg={6} className="ingredient-info-area">
-            <div className="ingredient-name">{selectedIngredient.name}</div>
-            <div className="ingredient-price">
-              <span className="discount-rate">30% </span>
-              {currencyFormat(selectedIngredient.price)}원
-            </div>
-            <div className="ingredient-info">
-              {selectedIngredient.description}
-            </div>
-            <div className="delivery-info">
-              <div>배송안내</div>
-              <h6>{address}</h6>
-              <AddressInput setAddress={setAddress} />
-              <DeliveryEstimate address={address} />
-            </div>
-            <div>
-              <Button className="add-button" onClick={addCart}>
+    <>
+      {selectedIngredient._id ? (
+        <Container maxWidth="lg" sx={{ padding: "50px 0" }}>
+          <Grid container spacing={4}>
+            <Grid item lg={6} xs={12} sx={{ textAlign: "center" }}>
+              <img
+                src={selectedIngredient.image}
+                alt={selectedIngredient.name}
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
+            </Grid>
+            <Grid item lg={6} xs={12}>
+              <Typography variant="h4" component="div" gutterBottom>
+                {selectedIngredient.name}
+              </Typography>
+              <Typography variant="h6" component="div">
+                <span style={{ color : "orangered"}}>30% </span>
+                {currencyFormat(selectedIngredient.price)}원
+              </Typography>
+              <Typography variant="body1" component="div" sx={{ mt: 2 }}>
+                {selectedIngredient.description}
+              </Typography>
+              <Box sx={{ mt: 4, borderTop: "1px solid #ddd", pt: 2 }}>
+                <Typography variant="body2" gutterBottom>
+                  배송안내
+                </Typography>
+                <Typography variant="h6">{address}</Typography>
+                <AddressInput setAddress={setAddress} />
+                <DeliveryEstimate address={address} />
+              </Box>
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ mt: 2 }}
+                onClick={addCart}
+              >
                 카트에 담기
               </Button>
-            </div>
-          </Col>
-          <div className="recipe-detail-reviews">
-            <Review type="ingredient" itemId={selectedIngredient._id} />
-          </div>
-        </Row>
-      </Container>
-    </div>
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 4 }}>
+              <Review type="ingredient" itemId={selectedIngredient._id} />
+            </Grid>
+          </Grid>
+        </Container>
+      ) : (
+        <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "200px"}}>
+          <Typography variant="h2">해당 상품이 존재하지 않습니다. 😅</Typography>
+          <Button variant='outlined' onClick={goHome} sx={{width: "700px", my: "20px"}}>홈으로 이동</Button>
+        </Container>
+      )}
+    </>
   );
 };
 
