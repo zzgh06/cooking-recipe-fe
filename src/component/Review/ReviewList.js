a792b6201f24ed2380f0020d85e098836293f741
zzgh06 <zzgh06@naver.com>
2024-07-12 18:55:38 +0900

    update-review-exception

--- src/component/Review/ReviewList.js
+++ src/component/Review/ReviewList.js
@@ -5,10 +5,11 @@ import ReviewStars from "./ReviewStars";
 const ReviewList = ({ type, reviews, userId, onEdit, onDelete }) => {
   return (
     <Box>
-      {reviews.map((review) => (
+      {reviews.length === 0 ? (
         <Box
-          key={review._id}
           sx={{
+            width: "100%",
+            height: "100px",
             border: "1px solid #ccc",
             p: 2,
             mb: 2,
@@ -19,44 +20,68 @@ const ReviewList = ({ type, reviews, userId, onEdit, onDelete }) => {
           <Box
             sx={{
               display: "flex",
+              justifyContent: "center",
               alignItems: "center",
-              gap: 1,
+              p: 2,
             }}
           >
-            <Typography variant="body1">
-              평점:
-            </Typography>
-            <ReviewStars startNum={review.rating} />
-            <Typography variant="body2" component="span">
-              ({new Date(review.createdAt).toLocaleString()})
-            </Typography>
-            <Typography variant="body2" sx={{ ml: "auto" }}>
-              작성자: {review.userId.id}
-            </Typography>
-          </Box>
-          <Box sx={{ mt: 1, display: "flex", alignItems: "center" }}>
-            <Typography variant="body1">{review.comment}</Typography>
-            {userId && userId.user && userId.user._id === review.userId._id && (
-              <Box sx={{ ml: "auto", display: "flex", gap: 1 }}>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => onDelete(review._id, type)}
-                >
-                  삭제
-                </Button>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  onClick={() => onEdit(review)}
-                >
-                  수정
-                </Button>
-              </Box>
-            )}
+            <Typography variant="h6" component="p">작성된 리뷰가 없습니다.</Typography>
           </Box>
         </Box>
-      ))}
+      ) : (
+        reviews.map((review) => (
+          <Box
+            key={review._id}
+            sx={{
+              border: "1px solid #ccc",
+              p: 2,
+              mb: 2,
+              borderRadius: 1,
+              backgroundColor: "#f9f9f9",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+              }}
+            >
+              <Typography variant="body1">평점:</Typography>
+              <ReviewStars startNum={review.rating} />
+              <Typography variant="body2" component="span">
+                ({new Date(review.createdAt).toLocaleString()})
+              </Typography>
+              <Typography variant="body2" sx={{ ml: "auto" }}>
+                작성자: {review.userId.id}
+              </Typography>
+            </Box>
+            <Box sx={{ mt: 1, display: "flex", alignItems: "center" }}>
+              <Typography variant="body1">{review.comment}</Typography>
+              {userId &&
+                userId.user &&
+                userId.user._id === review.userId._id && (
+                  <Box sx={{ ml: "auto", display: "flex", gap: 1 }}>
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      onClick={() => onDelete(review._id, type)}
+                    >
+                      삭제
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      onClick={() => onEdit(review)}
+                    >
+                      수정
+                    </Button>
+                  </Box>
+                )}
+            </Box>
+          </Box>
+        ))
+      )}
     </Box>
   );
 };
