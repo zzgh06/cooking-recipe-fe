679abacc21a4be924be566989a5670e76b6fb360
zzgh06 <zzgh06@naver.com>
2024-07-18 15:00:29 +0900

    fix-kakao-share

--- src/component/KakaoShareButton/KakaoShareButton.js
+++ src/component/KakaoShareButton/KakaoShareButton.js
@@ -3,36 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShareFromSquare } from "@fortawesome/free-regular-svg-icons";
 import { Box } from "@mui/material";
 
-const KAKAO_JAVASCRIPT_KEY = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY;
-
-const initializeKakao = () => {
-  if (window.Kakao && !window.Kakao.isInitialized()) {
-    console.log("Initializing Kakao SDK...");
-    window.Kakao.init(KAKAO_JAVASCRIPT_KEY);
-  }
-};
-
 const KakaoShareButton = ({ recipeDetail }) => {
+  const KAKAO_JAVASCRIPT_KEY = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY;
+
   useEffect(() => {
-    initializeKakao();
-  }, []);
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      console.log("Initializing Kakao SDK...");
+      window.Kakao.init(KAKAO_JAVASCRIPT_KEY);
+    }
+  }, [KAKAO_JAVASCRIPT_KEY]);
 
   const shareToKakao = () => {
     if (window.Kakao) {
-      console.log("window.Kakao:", window.Kakao);
-      console.log("recipeDetail:", recipeDetail);
-      console.log("Current URL:", window.location.href);
-
       try {
         window.Kakao.Share.sendCustom({
           templateId: 109930,
           templateArgs: {
             title: recipeDetail?.name,
             description: recipeDetail?.description,
-            link: {
-              mobileWebUrl: window.location.href,
-              webUrl: window.location.href,
-            },
+            id: recipeDetail?._id,
           },
         });
       } catch (error) {
