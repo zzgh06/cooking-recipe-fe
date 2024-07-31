26d0bfc3daa9786cd2cf3a8ace3dc4a2bc6b6889
zzgh06 <zzgh06@naver.com>
2024-07-31 19:44:54 +0900

    optimization

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppRouter from "./router/AppRouter";
 import AppLayout from "./Layout/AppLayout";
 import ChannelService from "./ChannelService";
+import { useEffect } from "react";
 
 
 function App() {
-  ChannelService.loadScript();
-  ChannelService.boot({
-    pluginKey: "e9460f81-7a6f-442d-8f93-23a70f8e892b",
-    },
-  );
+  useEffect(() => {
+    requestIdleCallback(() => {
+      ChannelService.loadScript();
+      ChannelService.boot({
+        pluginKey: "e9460f81-7a6f-442d-8f93-23a70f8e892b",
+      });
+    });
+  }, []);
+
   return (
-      <AppLayout>
-       <AppRouter />
-      </AppLayout>
+    <AppLayout>
+      <AppRouter />
+    </AppLayout>
   );
 }
 
