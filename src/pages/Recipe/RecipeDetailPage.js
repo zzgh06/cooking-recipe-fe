f81be95e90e4ee3d9319c619509dd1cb6b9cf777
zzgh06 <zzgh06@naver.com>
2024-08-01 18:22:03 +0900

    recipe detail page code split

--- src/pages/Recipe/RecipeDetailPage.js
+++ src/pages/Recipe/RecipeDetailPage.js
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { Suspense, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import Review from "../../component/Review/Review";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCartShopping,
   faSearch,
   faBookmark as solidBookmark,
 } from "@fortawesome/free-solid-svg-icons";
-import {
-  faBookmark as regularBookmark,
-} from "@fortawesome/free-regular-svg-icons";
+import { faBookmark as regularBookmark } from "@fortawesome/free-regular-svg-icons";
 import RecipeCategory from "../../component/RecipeCategory/RecipeCategory";
 import {
   Box,
@@ -26,21 +23,54 @@ import {
   TableHead,
   TableRow,
   Paper,
+  CircularProgress,
 } from "@mui/material";
 import KakaoShareButton from "../../component/KakaoShareButton/KakaoShareButton";
 import CopyClipButton from "../../component/CopyClipButton/CopyClipButton";
-import IngredientDialog from "../../component/IngredientDialog/IngredientDialog";
 import RecipeDetailSkeleton from "../../component/Skeleton/RecipeDetailSkeleton";
-import ShoppingListDialog from "../../component/ShoppingListDialog/ShoppingListDialog";
 import { useFetchRecipeById } from "../../hooks/Recipe/useFetchRecipeById";
 import { useRecipeFavorite } from "../../hooks/Favorite/useRecipeFavorite";
 import { useAddRecipeFavorite } from "../../hooks/Favorite/useAddRecipeFavorite";
 import { useDeleteRecipeFavorite } from "../../hooks/Favorite/useDeleteRecipeFavorite";
 
-const RecipeImage = styled("img")({
+const Review = React.lazy(() => import("../../component/Review/Review"));
+const IngredientDialog = React.lazy(() =>
+  import("../../component/IngredientDialog/IngredientDialog")
+);
+const ShoppingListDialog = React.lazy(() =>
+  import("../../component/ShoppingListDialog/ShoppingListDialog")
+);
+
+const ImageContainer = styled(Box)({
+  position: "relative",
   width: "100%",
+  maxHeight: "600px",
+  overflow: "hidden",
   borderRadius: "8px",
   marginBottom: "20px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const RecipeImage = styled("img")({
+  width: "100%",
+  height: "auto",
+  display: "block",
+});
+
+const PlaceholderImage = styled("div")({
+  width: "100%",
+  minWidth: "852px",
+  height: "100%",
+  minHeight: "600px",
+  borderRadius: "8px",
+  backgroundColor: "#f0f0f0",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "20px",
+  color: "#ccc",
 });
 
 const RecipeInfoContainer = styled(Box)({
@@ -78,23 +108,29 @@ const StyledButton = styled(Button)({
 const RecipeStepContainer = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
+  marginBottom: "10px",
+  width: "100%",
 });
 
-const Steps = styled("div")({
+const Steps = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "baseline",
   height: "187px",
-});
+  [theme.breakpoints.down("sm")]: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+}));
 
 const RecipeDetail = () => {
   const { id } = useParams();
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector((state) => state.auth.user)
-  const { data: recipeDetail, isLoading, isError } = useFetchRecipeById(id);
+  const user = useSelector((state) => state.auth.user);
+  const { data: recipeDetail, isLoading } = useFetchRecipeById(id);
   const [isFavorite, setIsFavorite] = useState(false);
   const [openIngredientDialog, setOpenIngredientDialog] = useState(false);
   const [openShoppingListDialog, setOpenShoppingListDialog] = useState(false);
+  const [imageLoading, setImageLoading] = useState(true);
   const { data: recipeFavorite } = useRecipeFavorite();
   const { mutate: addRecipeFavorite } = useAddRecipeFavorite();
   const { mutate: deleteRecipeFavorite } = useDeleteRecipeFavorite();
@@ -107,7 +143,7 @@ const RecipeDetail = () => {
 
   const handleFavoriteClick = () => {
     if (!user) {
-      navigate("/login")
+      navigate("/login");
     }
     if (isFavorite) {
       deleteRecipeFavorite(recipeDetail._id);
@@ -155,19 +191,17 @@ const RecipeDetail = () => {
   };
 
   const optimizeMainImageUrl = (url) => {
-    return url?.replace(/\/upload\//, '/upload/c_fill,h_1704,w_1704,f_webp/');
+    return url?.replace(/\/upload\//, "/upload/c_fill,h_1704,w_1704,f_webp/");
   };
+
   const optimizeSubImageUrl = (url) => {
-    return url?.replace(/\/upload\//, '/upload/c_fill,h_200,w_200,f_webp/');
+    return url?.replace(/\/upload\//, "/upload/c_fill,h_200,w_200,f_webp/");
   };
 
   const optimizedMainImageUrl = optimizeMainImageUrl(recipeDetail?.images[0]);
 
-
   if (isLoading) {
-    return (
-      <RecipeDetailSkeleton />
-    );
+    return <RecipeDetailSkeleton />;
   }
 
   return (
@@ -176,14 +210,23 @@ const RecipeDetail = () => {
       <Container maxWidth="md" sx={{ marginTop: "80px" }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <RecipeImage
-              src={optimizedMainImageUrl}
-              alt={recipeDetail?.name}
-            />
+            <ImageContainer>
+              <RecipeImage
+                src={optimizedMainImageUrl}
+                alt={recipeDetail?.name}
+                onLoad={() => setImageLoading(false)}
+                onError={() => setImageLoading(false)}
+              />
+            </ImageContainer>
           </Grid>
           <Grid item xs={12}>
             <RecipeInfoContainer>
-              <Typography variant="h4" component="h2" fontWeight="600" fontSize="27px">
+              <Typography
+                variant="h4"
+                component="h2"
+                fontWeight="600"
+                fontSize="27px"
+              >
                 {recipeDetail?.name}
               </Typography>
               <Box sx={{ display: "flex" }}>
@@ -228,13 +271,19 @@ const RecipeDetail = () => {
               </Typography>
             </HeadContainer>
             <RecipeIngredientButton>
-              <StyledButton variant="outlined" onClick={handleClickOpenIngredientDialog}>
+              <StyledButton
+                variant="outlined"
+                onClick={handleClickOpenIngredientDialog}
+              >
                 <FontAwesomeIcon icon={faSearch} />
                 <Box component="span" sx={{ ml: 1 }}>
                   재료검색
                 </Box>
               </StyledButton>
-              <StyledButton variant="outlined" onClick={handleClickOpenShoppingListDialog}>
+              <StyledButton
+                variant="outlined"
+                onClick={handleClickOpenShoppingListDialog}
+              >
                 <FontAwesomeIcon icon={faCartShopping} />
                 <Box component="span" sx={{ ml: 1 }}>
                   장보기
@@ -289,7 +338,12 @@ const RecipeDetail = () => {
                   <Typography variant="h3" component="h3">
                     {index + 1}.
                   </Typography>
-                  <Typography variant="h6" component="span" ml={2} fontWeight="600">
+                  <Typography
+                    variant="h6"
+                    component="span"
+                    ml={2}
+                    fontWeight="600"
+                  >
                     {step?.description}
                   </Typography>
                 </Steps>
@@ -297,27 +351,36 @@ const RecipeDetail = () => {
                   <RecipeImage
                     src={optimizeSubImageUrl(step?.image)}
                     alt={step?._id}
-                    sx={{ ml: 2, width: "200px", height: "auto" }}
+                    sx={{
+                      ml: 2,
+                      width: "200px",
+                      height: "auto",
+                      maxHeight: "200px",
+                    }}
                   />
                 )}
               </RecipeStepContainer>
             ))}
           </Grid>
           <Grid item xs={12}>
-            <Review type="recipe" itemId={recipeDetail?._id} />
+            <Suspense fallback={<CircularProgress />}>
+              <Review type="recipe" itemId={recipeDetail?._id} />
+            </Suspense>
           </Grid>
         </Grid>
       </Container>
-      <IngredientDialog
-        open={openIngredientDialog}
-        handleClose={handleCloseIngredientDialog}
-        ingredients={recipeDetail?.ingredients}
-      />
-      <ShoppingListDialog
-        open={openShoppingListDialog}
-        handleClose={handleCloseShoppingListDialog}
-        ingredients={recipeDetail?.ingredients}
-      />
+      <Suspense fallback={<CircularProgress />}>
+        <IngredientDialog
+          open={openIngredientDialog}
+          handleClose={handleCloseIngredientDialog}
+          ingredients={recipeDetail?.ingredients}
+        />
+        <ShoppingListDialog
+          open={openShoppingListDialog}
+          handleClose={handleCloseShoppingListDialog}
+          ingredients={recipeDetail?.ingredients}
+        />
+      </Suspense>
     </>
   );
 };
